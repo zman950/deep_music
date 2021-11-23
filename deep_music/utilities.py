c6124121cfe0da4d707b14bd4dfaad0155535633
Anna Vaugrante <a.vaugrante@gmail.com>
2021-11-23 14:25:07 +0000

    function created to extract key and mode of each midi files

---
+++ deep_music/utilities.py
@@ -0,0 +1,22 @@
+import music21
+import os
+
+def key_finder(filepath):
+    '''function that returns dictionnary tuple of key name and mode
+    of midi songs.
+    If fails return none'''
+    file_dict = {}
+    directory = os.listdir('../raw_data/Webscrapping/snes')
+    for filepath in directory:
+        try:
+            s = '../raw_data/Webscrapping/snes/'
+            score = music21.converter.parse(s+filepath)
+            key = score.analyze('key')
+            file_dict[filepath] = (key.tonic.name, key.mode)
+        except:
+            file_dict[filepath] = 'None'
+    return file_dict
+
+
+if __name__ == '__main__':
+    print(key_finder('/Users/annavaugrante/code/zman950/deep_music/raw_data/Webscrapping/snes'))
